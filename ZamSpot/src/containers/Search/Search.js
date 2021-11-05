7e93f5004ff10ef676d4658e30cdd5d7fc2459a5
YoungMin <miniyoung37@gmail.com>
2021-11-05 23:46:02 +0900

    :zap: 주소검색 기능 구현 중

--- ZamSpot/src/containers/Search/Search.js
+++ ZamSpot/src/containers/Search/Search.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { StyleSheet, View, TextInput, TouchableOpacity } from 'react-native';
 import { Header } from 'react-native-elements/dist/header';
 import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { unit, getHitSlop } from '@stylesheets';
 import { useTranslation } from 'react-i18next';
-import { searchAddress } from '../../apiActions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import _ from 'lodash';
+import { apiKey } from '../../apiKey';
 
 const Search = () => {
   const [searchData, setSearchData] = useState(null);
@@ -17,12 +18,6 @@ const Search = () => {
 
   const handleClickGoBack = () => {
     navigation.goBack(null);
-  };
-
-  const handleChangeKeyword = text => {
-    const response = searchAddress(text);
-    console.log(`response@@@`, response);
-    setKeyword(text);
   };
 
   const RenderHeaderLeft = () => {
@@ -47,11 +42,43 @@ const Search = () => {
       />
 
       <View style={styles.inputContainer}>
-        <TextInput
-          value={keyword}
-          onChangeText={handleChangeKeyword}
+        <GooglePlacesAutocomplete
+          minLength={2}
+          autoFocus={false}
+          listViewDisplayed="auto"
           placeholder={i18n.t('search')}
-          style={styles.searchTextInput}
+          returnKeyType={'default'}
+          fetchDetails
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(data, details);
+          }}
+          query={{
+            key: 'AIzaSyA74DAP3R0oCQeqpt1gcfH-HmwhPB7BAUc',
+            language: 'en',
+          }}
+          // requestUrl={{
+          //   url: 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api',
+          //   useOnPlatform: 'web',
+          // }}
+          // currentLocation
+          // currentLocationLabel="Current location"
+          onFail={error => console.error(error)}
+          getDefaultValue={() => ''}
+          debounce={20}
+          styles={{
+            textInputContainer: {
+              alignItems: 'center',
+              justifyContent: 'center',
+            },
+            textInput: {
+              height: unit(44),
+              fontSize: 18,
+            },
+            predefinedPlacesDescription: {
+              color: '#1faadb',
+            },
+          }}
         />
       </View>
       <View style={styles.container}></View>
@@ -71,18 +98,22 @@ const styles = StyleSheet.create({
     borderBottomColor: 'rgba(0,0,0,0.2)',
     borderBottomWidth: unit(1),
     paddingLeft: unit(20),
+    marginBottom: unit(10),
   },
   headerLeftContainer: {
     justifyContent: 'center',
     width: unit(20),
   },
   inputContainer: {
-    position: 'absolute',
-    top: unit(40),
-    left: unit(0),
-    paddingLeft: unit(80),
+    // position: 'absolute',
+    // top: unit(44),
+    // right: unit(0),
+    paddingLeft: unit(12),
     paddingRight: unit(12),
     width: '100%',
+    height: '100%',
+    flex: 1,
+    backgroundColor: '#fff',
   },
   searchTextInput: {
     width: '100%',
